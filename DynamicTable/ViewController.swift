6098d8b4f41b431690a63526327a4add2939b1b1
Ildar Zalyalov <ildarzalyalovmos@gmail.com>
2019-10-09 15:47:36 +0300

    Initial Commit

---
+++ DynamicTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DynamicTable
+//
+//  Created by Ильдар Залялов on 09/10/2019.
+//  Copyright © 2019 Ildar Zalyalov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
