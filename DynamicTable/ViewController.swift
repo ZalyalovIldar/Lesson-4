be4ab638f5ef36dabc7ab1a3b424d7f5275407e4
Ildar Zalyalov <ildarzalyalovmos@gmail.com>
2019-10-11 11:19:40 +0300

    lesson commit

--- DynamicTable/ViewController.swift
+++ DynamicTable/ViewController.swift
@@ -8,13 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.estimatedRowHeight = 100
+        textField.isSecureTextEntry = true
+        
+        textField.delegate = self
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+       
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellIdentifier") as! CustomTableViewCell
+        
+        
+        if indexPath.row % 2 == 0 {
+             cell.configure(with: "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.")
+        }
+        else {
+             cell.configure(with: "Hello world!")
+        }
+        
+        return cell 
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+        
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+       
+        
+    }
 }
 
