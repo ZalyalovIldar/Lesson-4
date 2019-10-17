48ff6b1b9a97315ea2a0a65f5192668062781a35
Nikita <luzhbinnikita@gmail.com>
2019-10-17 23:02:02 +0300

    final

--- lesson4/SceneDelegate.swift
+++ lesson4/SceneDelegate.swift
@@ -1,56 +1,56 @@
-//
-//  SceneDelegate.swift
-//  lesson4
-//
-//  Created by Никита Лужбин on 11/10/2019.
-//  Copyright © 2019 Никита. All rights reserved.
-//
-
 import UIKit
+import RealmSwift
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    
+    let realm = try! Realm()
+    var isAuthorized: String!
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        isAuthorized = UserDefaults.standard.string(forKey: "lastUser")
+
+        if UserDefaults.standard.string(forKey: "isFirstLaunch") == nil {
+                      
+            let users = Generator.generateUsers()
+            try! realm.write {
+                realm.add(users)
+            }
+            UserDefaults.standard.set("firstLaunch", forKey: "isFirstLaunch")
+        }
+        
+        if let userID = UserDefaults.standard.string(forKey: "lastUser") {
+            
+            let userID = userID
+            let user = getUserByID(ID: userID)!
+            
+            let storyboard = UIStoryboard(name: "UserWall", bundle: nil)
+            let navController = storyboard.instantiateInitialViewController() as! UINavigationController
+            let viewController = navController.children.first as! PostsTableViewController
+            viewController.user = user
+            
+            self.window?.rootViewController = navController
+            
+        } else {
+            
+            let storyboard = UIStoryboard(name: "Authorization", bundle: nil)
+            let viewController = storyboard.instantiateInitialViewController() as! AuthorizationViewController
+            self.window?.rootViewController = viewController
+        }
     }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
+    
+    func getUserByID(ID: String) -> User? {
+        
+        let users = realm.objects(User.self)
+        
+        for user in users {
+            if user.userID == ID { return user }
+        }
+        return nil
     }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-
-        // Save changes in the application's managed object context when the application transitions to the background.
-        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
-    }
-
-
 }
 
